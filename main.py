5430673365d5ee39b9f047c693eb1da072b230f3
Hugo Messias <hugomessias9@gmail.com>
2024-02-04 07:54:51 -0300

    added way of filling images too small on the Y or X axis with white pixels to avoid big distortions when resizing at the end

--- main.py
+++ main.py
@@ -29,11 +29,25 @@ if __name__ == "__main__":
         crop1, mrgn_ltr, mrgn_rtl, side = cropX(resultY)
         result = mkMarginX(crop1, page, x_margin, side, mrgn_ltr, mrgn_rtl)
 
-        final_h = best_w+2*y_margin
-        final_w = best_h+3*x_margin
+        final_h = best_h+2*y_margin
+        final_w = best_w+3*x_margin
         # cv.imwrite("results/__resultPrescale.png", result)
 
-        result = cv.resize(result, (final_h-final_h%5, final_w-final_w%5))
+        res_h, res_w, ch = result.shape
+        if res_h < best_h:
+            print("Filled H")
+            fill = np.full((final_h, res_w, ch), [255, 255, 255], dtype=np.uint8)
+            fill[0:res_h, 0:res_w] = result
+            result = fill.copy()
+            cv.imwrite("results/__fillH.png", result)
+        if res_w < best_w:
+            print("Filled W")
+            res_h, res_w, ch = result.shape
+            fill = np.full((res_h, final_w, ch), [255, 255, 255], dtype=np.uint8)
+            fill[0:res_h, 0:res_w] = result
+            result = fill.copy()
+            cv.imwrite("results/__fillW.png", result)
+        result = cv.resize(result, (final_w-final_w%5, final_h-final_h%5), interpolation=cv.INTER_CUBIC)
 
         cv.imwrite(f"results/baloes/{str(i).zfill(5)}.png", result)
         cv.imwrite("results/__result.png", result)
