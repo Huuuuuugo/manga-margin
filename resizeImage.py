90189598e7b8d1247fa4b219e68ce672be932f72
Hugo Messias <hugomessias9@gmail.com>
2024-02-04 07:09:48 -0300

    resizeImg: changed interpolation method

--- resizeImage.py
+++ resizeImage.py
@@ -28,21 +28,11 @@ def resizeImg(img, best_h, best_w):
         dim_w = crop_imgX.shape[1]
 
     img_h, img_w, ch = img.shape
-    result = img.copy()
-
     scale_h = best_h - dim_h
     scale_w = best_w - dim_w
 
-    # print("H: ", img_h, dim_h, best_h, scale_h)
-    # print("W: ", img_w, dim_w, best_w, scale_w)
-    # if scale_h and scale_w:
-    #     prop = abs(abs(img_h/best_h)/abs(img_w/best_w))
-    #     if prop > 1.1 or prop < 0.9:
-    #         pass
-    #     print("h/h: ", img_h/best_h)
-    #     print("w/w: ", img_w/best_w)
-    #     print("dif: ", abs(abs(img_h/best_h)/abs(img_w/best_w)))
+    scaled = cv.resize(img, (img_w, img_h+scale_h), interpolation=cv.INTER_LINEAR) 
+    scaled = cv.resize(img, (img_w+scale_w, img_h), interpolation=cv.INTER_LINEAR)
+    # cv.imwrite("scaleds/__scaled.png", scaled)
 
-    result = cv.resize(img, (img_w, img_h+scale_h)) 
-    result = cv.resize(img, (img_w+scale_w, img_h))
-    return result
+    return scaled
